3d544bfc6616ae79ef2de53733b183b03e2577ed
Marten de Vries <m@rtendevri.es>
2021-05-04 16:08:37 +0200

    Basic (in-memory) reduce

--- chairdb/view.py
+++ chairdb/view.py
@@ -10,10 +10,13 @@ from .utils import anext, verify_no_attachments
 
 
 class View:
-    def __init__(self, db, map, name=None):
+    def __init__(self, db, map, reduce=None, rereduce=None, name=None):
         self._db = db
         self._view_db = InMemoryDatabase('_view' + (name or uuid.uuid4().hex))
+
         self._map = map
+        self._reduce = reduce
+        self._rereduce = rereduce
 
     async def build(self):
         last_seq = await self._view_db.read_local('_local_seq')
@@ -63,10 +66,24 @@ class View:
             except NotFound:
                 # no such key yet, use it for the first time
                 new_doc = Document(full_key, rev_num=0, path=(), body={})
-        new_doc.body['value'] = value
-        new_doc.body['id'] = doc.id
+        new_doc.body = {'value': value}
         new_doc.update_rev()
         return new_doc
+
+    async def aggregate(self, *, start_key=None, end_key=None,
+                        descending=False, group_level=0):
+        query_results = self.query(start_key=start_key, end_key=end_key,
+                                   descending=descending)
+
+        current_key, current_values = None, []
+        async for (key, value, *_) in query_results:
+            grouped_key = None if group_level == 0 else key[:group_level]
+            if grouped_key != current_key:
+                if current_values:
+                    yield current_key, self._rereduce(current_values)
+                current_key, current_values = grouped_key, []
+            current_values.append(self._reduce([key], [value]))
+        yield current_key, self._rereduce(current_values)
 
     async def query(self, **opts):
         verify_no_attachments(opts.get('doc_opts', {}))
@@ -77,35 +94,34 @@ class View:
 
     @contextlib.asynccontextmanager
     async def query_with_attachments(self, *, start_key=None, end_key=None,
-                                     descending=False, reduce=False,
-                                     group_level=0, doc_opts={}):
+                                     descending=False, doc_opts=None):
         # start_key and end_key can be tuples of arity 2: (key, doc_id)
         if isinstance(start_key, str):
             start_key = [start_key]
         if start_key:
             start_key = complex_key(start_key)
         if isinstance(end_key, str):
-            end_key = [end_key, {}]  # TODO: good enough?
+            end_key = [end_key, {}]
         if end_key:
             end_key = complex_key(end_key)
 
-        # TODO: support these options
-        # TODO: some include_docs like thing? (read_opts here as well?) Re-use
-        # the contextmanager of this function somehow for the read() calls...
-        assert not reduce and group_level == 0
-
         await self.build()
 
-        async with self._view_db.read_transaction() as t:
-            all = t.all_docs(start_key=start_key, end_key=end_key,
-                             descending=descending, doc_opts={'body': True,
-                                                              'atts': False})
-            yield self._transform(t, all)
+        t_call = self._db.read_transaction()
+        vt_call = self._view_db.read_transaction()
+        async with t_call as t, vt_call as vt:
+            all = vt.all_docs(start_key=start_key, end_key=end_key,
+                              descending=descending, doc_opts={'body': True,
+                                                               'atts': False})
+            yield self._transform(t, all, doc_opts)
 
-    async def _transform(self, t, view_resp):
+    async def _transform(self, t, view_resp, doc_opts):
         async for v_doc in view_resp:
-            key = parse_complex_key(v_doc.id)
-            yield QueryResult(key[0], v_doc['value'], v_doc['id'])
+            key, id = parse_complex_key(v_doc.id)
+            doc = None
+            if doc_opts is not None:
+                doc = await anext(t.read(id, **doc_opts))
+            yield QueryResult(key, v_doc['value'], id, doc)
 
 
 class QueryResult(typing.NamedTuple):
--- tests/test_view.py
+++ tests/test_view.py
@@ -11,12 +11,22 @@ def map_title(doc):
     yield doc['title'], None
 
 
+def reduce(keys, values):
+    return len(values)
+
+
+def rereduce(values):
+    return sum(values)
+
+
 async def test_view():
     db = InMemoryDatabase()
-    view = View(db, map_title)
+    view = View(db, map_title, reduce, rereduce)
 
-    await db.write(Document('test', 1, ('a',), {'title': 'mytest'}))
-    await db.write(Document('test2', 1, ('b',), {'title': 'Hello World'}))
+    doc1 = Document('test', 1, ('a',), {'title': 'mytest'})
+    doc2 = Document('test2', 1, ('b',), {'title': 'Hello World'})
+    await db.write(doc1)
+    await db.write(doc2)
     assert await to_list(view.query()) == [
         QueryResult(key='Hello World', value=None, id='test2', document=None),
         QueryResult(key='mytest', value=None, id='test', document=None)
@@ -24,8 +34,21 @@ async def test_view():
     assert await to_list(view.query(start_key='m', end_key='n')) == [
         QueryResult(key='mytest', value=None, id='test', document=None)
     ]
-    await db.write(Document('test2', 2, ('c', 'b'), {'title': 'Hello World!'}))
+    doc3 = Document('test2', 2, ('c', 'b'), {'title': 'Hello World!'})
+    await db.write(doc3)
     assert await to_list(view.query()) == [
         QueryResult(key='Hello World!', value=None, id='test2', document=None),
         QueryResult(key='mytest', value=None, id='test', document=None)
     ]
+    # with document
+    assert await to_list(view.query(doc_opts={})) == [
+        QueryResult(key='Hello World!', value=None, id='test2', document=doc3),
+        QueryResult(key='mytest', value=None, id='test', document=doc1),
+    ]
+    assert await to_list(view.aggregate()) == [(None, 2)]
+
+    # maybe a list example would be better
+    group_result = [('He', 1), ('my', 1)]
+    assert await to_list(view.aggregate(group_level=2)) == group_result
+
+    assert await to_list(view.aggregate(start_key='z')) == [(None, 0)]
